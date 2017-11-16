5e0a063b6c80884fcb2d814a8155e4b3d7391de2
Dan Meliza <dan@meliza.org>
2017-11-16 11:21:47 -0500

    add some missing modules

---
+++ dstrf/performance.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""Functions for quantifying model performance"""
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+import pyspike as spk
+
+
+def corrcoef(a, b, downsample=None, smooth=None):
+    """Correlation coefficient between PSTHs
+
+    a, b:       spike vectors to compare (nbins, ntrials).
+                Must be same number of bins
+    downsample: factor by which to downsample the PSTHs
+    smooth:     if not None, smooth the downsampled PSTHs
+
+    """
+    from spikes import psth
+    p1 = psth(a, downsample, smooth)
+    p2 = psth(b, downsample, smooth)
+    return np.corrcoef(p1, p2)[0][1]
+
+
+def evenoddcorr(spikes, duration, smooth=1, dsample=10):
+    """The correlation between PSTHs of even and odd trials"""
+    strains = [spk.SpikeTrain(s, duration) for s in spikes]
+    evens = strains[::2]
+    odds = strains[1::2]
+    even_psth = psth_spiky(evens, smooth=smooth, dsample=dsample)
+    odd_psth = psth_spiky(odds, smooth=smooth, dsample=dsample)
+    return np.corrcoef(even_psth, odd_psth)[0][1]
---
+++ dstrf/spikes.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""Functions for manipulating spike trains"""
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+import pyspike as spk
+
+
+def psth(spike_v, downsample=None, smooth=None):
+    """Compute psth from multi-trial spike vector (dimension nbins x ntrials)
+
+    downsample: if not None, factor by which to downsample the PSTH
+    smooth:     if not None, smooth the downsampled PSTH
+    """
+    from scipy.ndimage.filters import gaussian_filter1d
+
+    nbins, ntrials = spike_v.shape
+    if downsample is not None:
+        psth = np.sum(spike_v.reshape(nbins // downsample, ntrials, -1), axis=(1, 2))
+    else:
+        psth = np.sum(spike_v, axis=1)
+    if smooth is not None:
+        return gaussian_filter1d(psth, smooth, mode="constant", cval=0.0)
+    else:
+        return psth

8871235fed3bfeb09e8ccb5a9f50bc70f25db33b
= <tdr5wc@virginia.edu>
2018-05-30 00:37:05 -0400

    updated batch files and removed windows line endings

---
+++ batch/ml_batch.sh
@@ -0,0 +1,13 @@
+SCRIPT=$1
+YAML=$2
+CELLS=$3
+SAVEROOT=$4
+MLTAG=$5
+
+. /scratch/tyler/dstrf/venv/bin/activate
+
+mkdir $SAVEROOT
+parallel -a $CELLS mkdir $SAVEROOT/{}/
+
+OMP_NUM_THREADS=1 parallel -a $CELLS python3 $SCRIPT {} $YAML $SAVEROOT/{} $MLTAG '>' $SAVEROOT/{}/{}-$MLTAG.log
+echo -n $$ DONE ' ' ; date "+%F %T"

0310e8ece80166143ff4fb3b65ab843ce2d84a94
Dan Meliza <dan@meliza.org>
2019-03-08 23:40:41 -0500

    update batch script

--- batch/univariate.sh
+++ batch/univariate.sh
@@ -3,4 +3,4 @@
 . venv/bin/activate
 export OMP_NUM_THREADS=1
 
-scripts/glm_univariate_biocm_emcee.py config/univariate_posp.yml
+python scripts/glm_univariate_biocm_emcee.py config/univariate_posp.yml

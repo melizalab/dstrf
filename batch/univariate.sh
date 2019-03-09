779fed1ed04d3e8c217ebba6879e02ffb03a8aa0
Dan Meliza <dan@meliza.org>
2019-03-09 08:29:16 -0500

    add all univariate models

--- batch/univariate.sh
+++ batch/univariate.sh
@@ -4,3 +4,5 @@
 export OMP_NUM_THREADS=1
 
 python scripts/glm_univariate_biocm_emcee.py config/univariate_posp.yml
+python scripts/glm_univariate_biocm_emcee.py config/univariate_tonic.yml
+python scripts/glm_univariate_biocm_emcee.py config/univariate_phasic.yml

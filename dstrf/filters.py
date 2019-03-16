fix hg filter normalization

dstrf/filters.py
@@ -75,9 +75,9 @@ def hg(nfreq, ntau, f_max, f_peak, t_peak, ampl, t_sigma, f_sigma, t_omega, f_om
     tprime, fprime = np.meshgrid(tc, fc)
     H = np.exp(-0.5 * ((tprime) / t_sigma)**2) * np.cos(2 * np.pi * t_omega * (tprime) + Pt)
     G = np.exp(-0.5 * ((fprime) / f_sigma)**2) * np.cos(2 * np.pi * f_omega * (fprime) + Pf)
-    strf = ampl * H * G
+    strf = H * G
     strf /= np.sqrt(np.trapz(np.trapz(strf**2, axis=1)))
-    return np.fliplr(strf), tscale, f
+    return np.fliplr(ampl * strf), tscale, f
 
 
 def gabor(nfreq, ntau, f_max, f_peak, t_peak, ampl, f_sigma, t_sigma, theta, lmbda, psi, **kwargs):
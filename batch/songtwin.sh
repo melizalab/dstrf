b3cf6384843297b21ee1b85d883ef65a7150c53b
Dan Meliza <dan@meliza.org>
2019-03-25 15:53:25 -0400

    tweaks to songtwin batch

--- batch/songtwin.sh
+++ batch/songtwin.sh
@@ -4,7 +4,7 @@
 export OMP_NUM_THREADS=1
 export THEANO_FLAGS="base_compiledir=/scratch/dmeliza/.theano"
 
-APARAMS="--save-data config/song_dynamical.yml"
+APARAMS="--save-data"
 
 grep -v "^#" config/hg_filters.csv |
-    parallel --skip-first-line --colsep ',' echo python scripts/assimilate_simulated.py -k data.dynamics.model=models/{1}.yml -k data.filter.rf={2} $APARAMS results/{1}_{2}.npz
+    parallel --skip-first-line --colsep ',' echo python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_phasic.yml results/phasic_{1}.npz

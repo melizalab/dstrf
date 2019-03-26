b6e56b540e80a4613e0921d4a0ff26259560bf79
Dan Meliza <dan@meliza.org>
2019-03-26 10:07:37 -0400

    songtwin batch now does mcmc

--- batch/songtwin.sh
+++ batch/songtwin.sh
@@ -4,13 +4,13 @@
 export OMP_NUM_THREADS=1
 export THEANO_FLAGS="base_compiledir=/scratch/dmeliza/.theano"
 
-APARAMS="--save-data"
+APARAMS="--save-data --mcmc"
 
 grep -v "^#" config/hg_filters.csv |
-    parallel --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_phasic.yml results/phasic_{1}.npz
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_phasic.yml results/phasic_{1}.npz
 
 grep -v "^#" config/hg_filters.csv |
-    parallel --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_tonic.yml results/tonic_{1}.npz
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_tonic.yml results/tonic_{1}.npz
 
 grep -v "^#" config/hg_filters.csv |
-    parallel --skip-first-line --colsep ',' echo python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_posp.yml results/posp_{1}.npz
+    parallel -j 1 --skip-first-line --colsep ',' echo python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_posp.yml results/posp_{1}.npz

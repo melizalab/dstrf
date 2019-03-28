b732474ea20aabc21949497e960166520454c312
Dan Meliza <dan@meliza.org>
2019-03-28 15:37:20 -0400

    update batch params for xval fit

--- batch/songtwin.sh
+++ batch/songtwin.sh
@@ -4,13 +4,13 @@
 export OMP_NUM_THREADS=1
 export THEANO_FLAGS="base_compiledir=/scratch/dmeliza/.theano"
 
-APARAMS="--save-data --mcmc --skip-completed"
+APARAMS="--save-data --xval --skip-completed"
 
 grep -v "^#" config/hg_filters.csv |
-    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_phasic.yml results/phasic_{1}.npz
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.model.dynamics=models/phasic.yml $APARAMS config/song_dynamical.yml results/phasic_{1}_xval.npz
 
 grep -v "^#" config/hg_filters.csv |
-    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_tonic.yml results/tonic_{1}.npz
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.model.dynamics=models/tonic.yml $APARAMS config/song_dynamical.yml results/tonic_{1}_xval.npz
 
 grep -v "^#" config/hg_filters.csv |
-    parallel -j 1 --skip-first-line --colsep ',' echo python scripts/assimilate_simulated.py -k data.filter.rf={1} $APARAMS config/song_posp.yml results/posp_{1}.npz
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.model.dynamics=models/posp.yml $APARAMS config/song_dynamical.yml results/posp_{1}_xval.npz

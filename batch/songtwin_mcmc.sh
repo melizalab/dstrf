7f607dab9e0cbd81da916b64806e245396197621
Dan Meliza <dan@meliza.org>
2019-03-29 22:50:44 -0400

    switch assimilation to dstrf_sim data

---
+++ batch/songtwin_mcmc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. venv/bin/activate
+export OMP_NUM_THREADS=1
+export THEANO_FLAGS="base_compiledir=/scratch/dmeliza/.theano"
+
+APARAMS="--save-data --mcmc --skip-completed"
+
+grep -v "^#" config/hg_filters.csv |
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.dynamics.model=models/phasic.yml --restart results/phasic_{1}_xval.npz $APARAMS config/song_dynamical.yml results/phasic_{1}_samples.npz
+
+grep -v "^#" config/hg_filters.csv |
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.dynamics.model=models/tonic.yml --restart results/tonic_{1}_xval.npz $APARAMS config/song_dynamical.yml results/tonic_{1}_samples.npz
+
+grep -v "^#" config/hg_filters.csv |
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.dynamics.model=models/posp.yml --restart results/posp_{1}_xval.npz $APARAMS config/song_dynamical.yml results/posp_{1}_samples.npz

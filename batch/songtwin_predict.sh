b315978a6e65af112283683d9c30640ae7f0ec24
Dan Meliza <dan@meliza.org>
2019-07-23 11:28:48 -0400

    some fixes to batch predict script

--- batch/songtwin_predict.sh
+++ batch/songtwin_predict.sh
@@ -7,10 +7,10 @@ export OMP_NUM_THREADS=1
 APARAMS="-k data.trials=50"
 
 grep -v "^#" config/hg_filters.csv |
-    parallel --skip-first-line --colsep ',' echo python scripts/predict_simulated.py $APARAMS config/song_dynamical.yml results/phasic_{1}_samples.npz results/phasic_{1}_predict.npz
+    parallel --skip-first-line --colsep ',' python scripts/predict_simulated.py $APARAMS config/song_dynamical.yml results/phasic_{1}_samples.npz results/phasic_{1}_predict.npz
 
 grep -v "^#" config/hg_filters.csv |
-    parallel --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.dynamics.model=models/tonic.yml --restart results/tonic_{1}_xval.npz $APARAMS config/song_dynamical.yml results/tonic_{1}_samples.npz
+    parallel --skip-first-line --colsep ',' python scripts/predict_simulated.py $APARAMS config/song_dynamical.yml results/tonic_{1}_samples.npz results/tonic_{1}_predict.npz
 
 grep -v "^#" config/hg_filters.csv |
-    parallel --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.dynamics.model=models/posp.yml --restart results/posp_{1}_xval.npz $APARAMS config/song_dynamical.yml results/posp_{1}_samples.npz
+    parallel --skip-first-line --colsep ',' python scripts/predict_simulated.py $APARAMS config/song_dynamical.yml results/posp_{1}_samples.npz results/posp_{1}_predict.npz

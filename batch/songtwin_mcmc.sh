cdf5c8f3f2d59fbf56cda702eb23c5ddcc460d5b
Dan Meliza <dan@meliza.org>
2019-08-13 14:06:41 -0400

    fix current scaling for posp model

--- batch/songtwin_mcmc.sh
+++ batch/songtwin_mcmc.sh
@@ -13,4 +13,4 @@ grep -v "^#" config/hg_filters.csv |
     parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.dynamics.model=models/tonic.yml --restart results/tonic_{1}_xval.npz $APARAMS config/song_dynamical.yml results/tonic_{1}_samples.npz
 
 grep -v "^#" config/hg_filters.csv |
-    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.dynamics.model=models/posp.yml --restart results/posp_{1}_xval.npz $APARAMS config/song_dynamical.yml results/posp_{1}_samples.npz
+    parallel -j 1 --skip-first-line --colsep ',' python scripts/assimilate_simulated.py -k data.filter.rf={1} -k data.dynamics.model=models/posp.yml -k data.dynamics.current_scaling=2.0 --restart results/posp_{1}_xval.npz $APARAMS config/song_dynamical.yml results/posp_{1}_samples.npz

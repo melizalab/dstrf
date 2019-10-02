bf5ac80bca751e52a65fa386bab52d2e00d53dd5
Dan Meliza <dan@meliza.org>
2019-10-02 14:15:37 -0400

    return to multithread mcmc; remove songtwin_test

--- batch/songtwin_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. venv/bin/activate
-export OMP_NUM_THREADS=1
-
-python scripts/assimilate.py -k data.filter.rf=10 -k data.dynamics.model=models/phasic.yml -k emcee.nwalkers=1000 -k emcee.nsteps=50000 -k emcee.startpos_scale=0.001 --restart results/20190402/phasic_10_xval.npz --save-data --mcmc --save-chain config/song_dynamical.yml phasic_10_samples.npz

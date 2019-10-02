751d70e500f4ae3c015f5d88d668dbb89705fc45
Dan Meliza <dan@meliza.org>
2019-10-02 13:44:33 -0400

    move model-specific code for univariate_dynamical into shell script

--- batch/univariate.sh
+++ batch/univariate.sh
@@ -2,9 +2,9 @@
 
 . venv/bin/activate
 export OMP_NUM_THREADS=1
-export THEANO_FLAGS="base_compiledir=/scratch/dmeliza/.theano"
+APARAMS="--mcmc --save-data --save-chain"
 
-python scripts/assimilate.py --mcmc --save-data config/univariate_glm.yml results/univariate_glm_samples.npz
-python scripts/assimilate.py --mcmc --save-data config/univariate_posp.yml results/univariate_posp_samples.npz
-python scripts/assimilate.py --mcmc --save-data config/univariate_tonic.yml results/univariate_tonic_samples.npz
-python scripts/assimilate.py --mcmc --save-data config/univariate_phasic.yml results/univariate_phasic_samples.npz
+python scripts/assimilate.py ${APARAMS} config/univariate_glm.yml results/univariate_glm_samples.npz
+python scripts/assimilate.py ${APARAMS} -k data.dynamics.model=models/posp.yml -k data.dynamics.current_scaling=2.0 config/univariate_dynamical.yml results/univariate_posp_samples.npz
+python scripts/assimilate.py ${APARAMS} -k data.dynamics.model=models/tonic.yml -k data.dynamics.current_scaling=4.0 config/univariate_dynamical.yml results/univariate_tonic_samples.npz
+python scripts/assimilate.py ${APARAMS} -k data.dynamics.model=models/phasic.yml -k data.dynamics.current_scaling=9.0 config/univariate_dynamical.yml results/univariate_phasic_samples.npz

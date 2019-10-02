069f76996bfa1bcb0f14a574761aa5ecaeb401d5
Dan Meliza <dan@meliza.org>
2019-10-02 15:03:08 -0400

    check_param_bounds will accept multiple files

--- scripts/check_param_bounds.py
+++ scripts/check_param_bounds.py
@@ -8,12 +8,12 @@ from dstrf import models
 import numpy as np
 from munch import Munch
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
 
     p = argparse.ArgumentParser(description="check if spike history parameter estimates are in bounds")
     p.add_argument("config", help="path to configuration yaml file")
-    p.add_argument("fitfile", help="path to output npz file")
+    p.add_argument("fitfile", help="path to output npz file(s)", nargs="+")
 
     args = p.parse_args()
     with open(args.config, "rt") as fp:
@@ -21,14 +21,15 @@ if __name__ == "__main__":
 
     matboundprior = models.matbounds(cf.model.ataus[0], cf.model.ataus[1], cf.model.t_refract)
 
-    results = np.load(args.fitfile)
-    if "samples" in results:
-        pos = results["samples"]
-        allowed = [matboundprior(s) for s in pos]
-        print("{}\t{}/{} samples in allowed region".format(args.fitfile, sum(allowed), len(allowed)))
-    else:
-        w0 = results["mle"]
-        if matboundprior(w0):
-            print("{}\tOK".format(args.fitfile))
+    for path in args.fitfile:
+        results = np.load(path)
+        if "samples" in results:
+            pos = results["samples"]
+            allowed = [matboundprior(s) for s in pos]
+            print("{}\t{}/{} samples in allowed region".format(path, sum(allowed), len(allowed)))
         else:
-            print("{}\tout of bounds".format(args.fitfile))
+            w0 = results["mle"]
+            if matboundprior(w0):
+                print("{}\tOK".format(path))
+            else:
+                print("{}\tout of bounds".format(path))

6fa81aacb760db37931f74a3a0296ccf09fd8050
Dan Meliza <dan@meliza.org>
2020-12-23 09:13:32 -0500

    update univariate script

--- batch/univariate.sh
+++ batch/univariate.sh
@@ -8,11 +8,10 @@ RESDIR="results/${DATE}_univariate"
 mkdir -p ${RESDIR}
 echo "univariate analysis - output to ${RESDIR}"
 
-
 # python scripts/assimilate.py ${APARAMS} config/univariate_glm.yml results/univariate_glm_samples.npz
-python scripts/assimilate.py ${APARAMS} -k data.dynamics.model=models/tonic.yml -k data.dynamics.current_scaling=4.0 config/univariate_dynamical.yml ${RESDIR}/univariate_tonic_samples.npz
-python scripts/assimilate.py ${APARAMS} -k data.dynamics.model=models/phasic.yml -k data.dynamics.current_scaling=9.0 config/univariate_dynamical.yml ${RESDIR}/univariate_phasic_samples.npz
+python scripts/assimilate.py ${APARAMS} config/univariate_tonic.yml ${RESDIR}/univariate_tonic_samples.npz
+python scripts/assimilate.py ${APARAMS} config/univariate_phasic.yml ${RESDIR}/univariate_phasic_samples.npz
 
 # python scripts/predict.py config/univariate_glm.yml results/univariate_glm_samples.npz results/univariate_glm_predict.npz
-python scripts/predict.py -k data.dynamics.model=models/tonic.yml -k data.dynamics.current_scaling=4.0 --save-data ${RESDIR}/univariate_tonic_predict.npz config/univariate_dynamical.yml ${RESDIR}/univariate_tonic_samples.npz
-python scripts/predict.py -k data.dynamics.model=models/phasic.yml -k data.dynamics.current_scaling=9.0 --save-data ${RESDIR}/univariate_phasic_predict.npz config/univariate_dynamical.yml ${RESDIR}/univariate_phasic_samples.npz
+python scripts/predict.py --save-data ${RESDIR}/univariate_tonic_predict.npz config/univariate_tonic.yml ${RESDIR}/univariate_tonic_samples.npz
+python scripts/predict.py --save-data ${RESDIR}/univariate_phasic_predict.npz config/univariate_phasic.yml ${RESDIR}/univariate_phasic_samples.npz
